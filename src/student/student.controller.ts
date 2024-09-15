57822ae0fbf9338734750eaea25d1f454dedc233
masabmunir <masabk214@gmail.com>
2024-09-16 04:17:24 +0500

    updating student controller

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { StudentDto } from './DTO/student-dto';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor( private readonly studentService:StudentService){}
+
+
+    @Post()
+    createStudent(@Body() studentDto:StudentDto){
+        return this.studentService.createStudent(studentDto);
+    }
+
+    @Get()
+    getStudent(){
+        return this.studentService.getAllStundent();
+    }
+
+    @Get(':id')
+    findStudent(@Param(':id') id:number){
+        return this.studentService.findOneStudent(id)
+    }
+}
