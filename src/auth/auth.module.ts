09123ac2b9365e8bda4e53d45fd751dd7dcb2086
masabmunir <masabk214@gmail.com>
2024-09-26 15:31:13 +0500

    adding modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { StudentModule } from 'src/student/student.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    JwtModule.register({
+      secret:'',
+      signOptions:{}
+    }),
+    StudentModule
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
