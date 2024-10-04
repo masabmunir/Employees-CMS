b9e9f592a823e40ad4acd2258fb266a0ba0a9a94
masabmunir <masabk214@gmail.com>
2024-10-04 17:54:12 +0500

    implemented profile route for jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,29 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards,Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { StudentDto } from 'src/student/DTO/student-dto';
+import { JwtAuthService } from './jwt-auth/jwt-auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService:AuthService){}
+    constructor(private authService: AuthService) { }
 
     @Post('register')
-   async register(@Body() studentDto:StudentDto){
-    return this.authService.register(studentDto)
+    async register(@Body() studentDto: StudentDto) {
+        return this.authService.register(studentDto)
     }
 
     @Post('login')
-    async login(@Body() body:{email:string, password:string}){
+    async login(@Body() body: { email: string, password: string }) {
         return this.authService.login(body.email, body.password);
     }
 
-    // @Get('profile') // New protected route
-    // @UseGuards(JwtAuthService) // Protect this route with JwtAuthGuard
-    // getProfile(@Request() req) {
-    //     return req.user; // Assuming you want to return the user info
-    // }
+    @Get('profile')
+    @UseGuards(JwtAuthService) 
+    getProfile(@Request() req:any) {
+        // Return the profile and success message
+        return {
+            message: 'Profile record fetched successfully',
+            user: req.user,
+        };
+    }
 }
