82a687ea5158f0814a87cdd1a0760278577051ff
masabmunir <masabk214@gmail.com>
2024-10-04 17:16:26 +0500

    jwt implemented

--- src/config/jwt.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from "@nestjs/config";
-
-export const JWT_CONFIG = registerAs("JWT", () => {
-  return {
-    SECRET: process.env["JWT_SECRET"],
-    EXPIRE_TIME: process.env["JWT_EXPIRE_TIME"],
-  };
-});
